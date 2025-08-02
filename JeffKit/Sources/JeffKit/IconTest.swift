c9de630f3073606eff59f09bddea0aa6c491316b
Jeff Cedilla <spaghetticoderapps@gmail.com>
2025-08-02 13:52:06 -0700

    assets

---
+++ JeffKit/Sources/JeffKit/IconTest.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+@available(iOS 14.0, *)
+public struct IconTestView: View {
+    public init() {}
+    
+    public var body: some View {
+        VStack(spacing: 20) {
+            Text("JeffKit Icon Test")
+                .font(.title)
+            
+            HStack(spacing: 15) {
+                ForEach(JeffApps.all, id: \.id) { app in
+                    VStack {
+                        Image(app.iconName, bundle: .module)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(10)
+                        
+                        Text(app.name)
+                            .font(.caption)
+                            .lineLimit(1)
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
